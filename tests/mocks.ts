579e9add03781bf9fe6758b6400cd5d2c8f5b5bb
Nicholas Visalli <nvisal1225@gmail.com>
2018-09-23 17:05:13 -0400

    const for mock objects

---
+++ tests/mocks.ts
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const MOCK_OBJECTS = {
+  USERNAME_QUERY: { username: 'nvisal1' },
+  EMPTY_USERNAME_QUERY: { username: '' },
+  USERNAME: 'cypress',
+  USERNAME_OTHER: 'nvisal1',
+  PASSWORD: 'Clarktesting1!',
+  EMAIL: 'test@test.com',
+  EMPTY_STRING: '',
+  LEARNING_OBJECT_NAME: 'test',
+  LEARNING_OBJECT_AUTHOR: 'nvisal1',
+  NAME: 'nick testing',
+  RATING: {
+    number:  4,
+    comment: 'unit test'
+  },
+  EDIT_RATING: {
+    number:  3,
+    comment: 'unit test edit'
+  },
+  CYPRESS_FLAG: {
+    comment: 'unit test flag',
+    username: 'cypress',
+    concern: 'unit test concern label'
+  },
+  FLAG: {
+    comment: 'unit test flag',
+    username: 'nvisal1',
+    concern: 'unit test concern label'
+  }
+};
