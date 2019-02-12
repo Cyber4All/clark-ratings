87836e2b487e2b0257c4bf16ad39d17977fd8418
Nick Visalli <nvisal1@students.towson.edu>
2019-02-12 16:20:20 -0500

    Add interactor spec files for both modules

---
+++ src/flags/FlagInteractor.spec.ts
@@ -0,0 +1,89 @@
+import { expect } from 'chai';
+import { MockDriver } from '../drivers/MockDriver';
+import { MOCK_OBJECTS } from '../tests/mocks';
+import * as interactor from './FlagInteractor';
+
+const driver = new MockDriver();
+
+let ratingId: string;
+
+describe('flagRating', () => {
+  it('Should return error - author of rating cannot perform this action!', done => {
+    return interactor.flagRating({
+      dataStore: driver,
+      ratingId,
+      currentUsername: MOCK_OBJECTS.USERNAME,
+      flag: MOCK_OBJECTS.FLAG,
+    }).then(val => {
+      console.log(val);
+      expect.fail();
+      done();
+    }).catch((error) => {
+      expect(error).to.be.a('string');
+      done();
+    });
+  });
+  it('Should flag the rating created during test 1', done => {
+    return interactor.flagRating({
+      dataStore: driver,
+      ratingId,
+      currentUsername: MOCK_OBJECTS.USERNAME_OTHER,
+      flag: MOCK_OBJECTS.FLAG,
+    }).then(val => {
+      expect(val).to.be.an('undefined');
+      done();
+    }).catch((error) => {
+      console.log(error);
+      expect.fail();
+      done();
+    });
+  });
+});
+
+describe('getAllFlags', () => {
+  it('Should return all flags - this is an admin operation', done => {
+    return interactor.getAllFlags({
+        dataStore: driver,
+    }).then(val => {
+      expect(val).to.be.an('array');
+      done();
+    }).catch((error) => {
+      console.log(error);
+      expect.fail();
+      done();
+    });
+  });
+});
+
+describe('getUserFlags', () => {
+  it('Should return all flags for a specified user - this is an admin operation', done => {
+    return interactor.getUserFlags({
+        dataStore: driver,
+        currentUsername: MOCK_OBJECTS.USERNAME,
+    }).then(val => {
+      expect(val).to.be.an('array');
+      done();
+    }).catch((error) => {
+      console.log(error);
+      expect.fail();
+      done();
+    });
+  });
+});
+
+describe('getRatingFlags', () => {
+  it('Should return all flags for a specified rating - this is an admin operation', done => {
+    return interactor.getRatingFlags({
+      dataStore: driver,
+      ratingId,
+    }).then(val => {
+      expect(val).to.be.an('array');
+      done();
+    }).catch((error) => {
+      console.log(error);
+      expect.fail();
+      done();
+    });
+  });
+});
+
---
+++ src/ratings/RatingsInteractor.spec.ts
@@ -0,0 +1,117 @@
+import { expect } from 'chai';
+import { MockDriver } from '../drivers/MockDriver';
+import { MOCK_OBJECTS } from '../tests/mocks';
+import * as interactor from './RatingsInteractor';
+
+const driver = new MockDriver();
+
+let ratingId: string;
+
+describe('createNewRating', () => {
+  it('Should create a new rating object', done => {
+    jest.setTimeout(30000);
+    return interactor.createRating({
+      dataStore: driver,
+      rating: MOCK_OBJECTS.RATING,
+      learningObjectId: MOCK_OBJECTS.LEARNING_OBJECT_ID,
+      username: MOCK_OBJECTS.USERNAME,
+      email: MOCK_OBJECTS.EMAIL,
+      name: MOCK_OBJECTS.NAME,
+    }).then(val => {
+      expect(val).to.be.an('undefined');
+      done();
+    }).catch((error) => {
+      console.log(error);
+      expect.fail();
+      done();
+    });
+  });
+});
+
+describe('getLearningObjectRatings', () => {
+  it('Should get rating created in first test', done => {
+    return interactor.getLearningObjectRatings({
+      dataStore: driver,
+      learningObjectId: MOCK_OBJECTS.LEARNING_OBJECT_ID,
+    }).then(val => {
+      expect(val).to.be.an('object');
+      done();
+    }).catch((error) => {
+      console.log(error);
+      expect.fail();
+      done();
+    });
+  });
+});
+
+describe('updateRating', () => {
+  it('Should throw error - only author of rating can do this', done => {
+    return interactor.updateRating({
+      dataStore: driver,
+      ratingId,
+      learningObjectId: MOCK_OBJECTS.LEARNING_OBJECT_ID,
+      updates: MOCK_OBJECTS.EDIT_RATING,
+      currentUsername: MOCK_OBJECTS.USERNAME_OTHER,
+    }).then(val => {
+      console.log(val);
+      expect.fail();
+      done();
+    }).catch((error) => {
+      expect(error).to.be.a('string');
+      done();
+    });
+  });
+  it('Should update the rating object created in first test', done => {
+    return interactor.updateRating({
+      dataStore: driver,
+      ratingId,
+      learningObjectId: MOCK_OBJECTS.LEARNING_OBJECT_ID,
+      updates: MOCK_OBJECTS.EDIT_RATING,
+      currentUsername: MOCK_OBJECTS.USERNAME,
+    }).then(val => {
+      expect(val).to.be.an('undefined');
+      done();
+    }).catch((error) => {
+      console.log(error);
+      expect.fail();
+      done();
+    });
+  });
+});
+
+describe('deleteRating', () => {
+  it('Should throw error - only the author of rating can do this', done => {
+    return interactor.deleteRating({
+      dataStore: driver,
+      ratingId,
+      learningObjectId: MOCK_OBJECTS.LEARNING_OBJECT_ID,
+      currentUsername: MOCK_OBJECTS.USERNAME_OTHER,
+    }).then(val => {
+      console.log(val);
+      expect.fail();
+      done();
+    }).catch((error) => {
+      expect(error).to.be.a('string');
+      done();
+    });
+  });
+  it('Should delete the rating created during test 1', done => {
+    return interactor.deleteRating({
+      dataStore: driver,
+      ratingId,
+      learningObjetId: MOCK_OBJECTS.LEARNING_OBJECT_ID,
+      currentUsername: MOCK_OBJECTS.USERNAME,
+    }).then(val => {
+      expect(val).to.be.an('undefined');
+      done();
+    }).catch((error) => {
+      console.log(error);
+      expect.fail();
+      done();
+    });
+  });
+});
+
+
+
+
