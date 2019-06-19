86578bbf88f10023b21a3a17257a27ecba102134
jbarlow96 <33383985+jbarlow96@users.noreply.github.com>
2019-06-19 14:56:21 -0400

    add JSDoc

--- src/flags/gateways/RatingGateway.ts
+++ src/flags/gateways/RatingGateway.ts
@@ -1,5 +1,9 @@
 import { getRating as fetchRating } from '../../ratings/RatingsInteractor';
 
+/**
+ * Grabs a rating object
+ * @param ratingId the id of the rating that is getting grabbed
+ */
 export async function getRating(ratingId: string) {
     return await fetchRating({
         ratingId,
